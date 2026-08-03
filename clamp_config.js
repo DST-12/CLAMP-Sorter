44d97b9cf6ee3ba9cf46b90ec1e4d5fd267304ea
DST <119148019+DST-12@users.noreply.github.com>
2026-08-03 15:49:30 -0600

    Delete clamp_config.js

--- clamp_config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    localStoragePrefix: "clamp-op-sorter",
-    title: "CLAMP Openings & Endings Sorter",
-    description: "¡Ordena las 76 canciones de openings y endings de todos los animes de CLAMP!"
-};
